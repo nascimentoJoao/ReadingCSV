bd69fbab197e157c431ed2eb75979c239f9e2dd4
nascimentoJoao <joaozinhusama@gmail.com>
2017-07-30 23:29:32 -0300

    Alterações realizadas em casa!

--- app/src/main/java/com/example/readingcsv/MainActivity.java
+++ app/src/main/java/com/example/readingcsv/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.readingcsv;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TableLayout;
 import android.widget.TextView;
 
@@ -25,6 +29,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -51,14 +57,40 @@ public class MainActivity extends AppCompatActivity {
         String dataAtual = formatDate.format(dateObject.getTime());
         TextView showData = (TextView) findViewById(R.id.showData);
         showData.setText(dataAtual);
+
         try {
 
             while ((line = reader.readNext()) != null){
                 switch(line[0]){
                     case "Disciplina":
+
                         String disciplina = line[1];
                         TextView showDisciplina = (TextView) findViewById(R.id.showDisciplina);
                         showDisciplina.setText(disciplina);
+                        break;
+
+                    case "Aluno":
+
+                        final List<String> meusAlunos = new ArrayList<String>();
+
+                        while((line = reader.readNext()) != null) {
+                            meusAlunos.add(line[0]);
+                        }
+                        ArrayAdapter<String> alunos = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, meusAlunos);
+                        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+                        listaAlunos.setAdapter(alunos);
+                        listaAlunos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                int pos = position;
+                                if (pos == meusAlunos.indexOf(meusAlunos)){
+                                    Intent intent = new Intent("android.intent.action.show_aluno.class");
+                                    startActivity(intent);
+                                }
+                            }
+                        });
+
+                        break;
                 }
             }
 
